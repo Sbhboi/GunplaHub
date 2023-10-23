fcee03e8728d1d985e494af5dbe9e9c8358438f6
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2023-10-23 15:40:19 +0800

    v1.3 - Added Post for logged in user, viewing all post in /posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!, only: [:create, :destroy]
+  
+    def index
+      @posts = Post.all
+    end
+    
+    def create
+      @post = current_user.posts.build(post_params)
+      if @post.save
+        redirect_to root_path, notice: 'Post created successfully.'
+      else
+        redirect_to root_path, alert: 'Post creation failed.'
+      end
+    end
+  
+    def destroy
+      @post = current_user.posts.find(params[:id])
+      @post.destroy
+      redirect_to root_path, notice: 'Post deleted successfully.'
+    end
+  
+    private
+  
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :posts, only: [:index, :create, :destroy]
   root to: "home#index"
 end
