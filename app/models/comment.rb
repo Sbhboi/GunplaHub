9917f3668857d6f9ea4936e943d4f6966d0dbbcc
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2023-10-24 23:47:33 +0800

    v1.5.1 - Added replying commenter function.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  has_many :replies, class_name: 'Comment', foreign_key: 'parent_comment_id', dependent: :destroy
+  belongs_to :parent_comment, class_name: 'Comment', optional: true
 end
